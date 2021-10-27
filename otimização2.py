b8d400e5890bc68b583e4cb95f03e3c052779881
ojsj18 <ojsj18@gmail.com>
2021-10-27 18:58:17 -0300

    pronto

--- otimização2.py
+++ otimização2.py
@@ -7,8 +7,13 @@ entrada = sys.stdin.read().split()
 n, e = int(entrada[0]), int(entrada[1]) #numero de nos e arestas
 del(entrada[0],entrada[0])
 
-ini = int(entrada[0])
-fim = int(entrada[1])
+if int(entrada[0])<int(entrada[1]):
+    ini = int(entrada[0])
+    fim = int(entrada[1])
+else:
+    ini = int(entrada[1])
+    fim = int(entrada[0])
+
 peso = int(entrada[2])
 del(entrada[0:3])
 
@@ -17,7 +22,7 @@ lista = np.copy(entrada)
 
 #b tem saida um apenas na entrada e na saida, onde a soma das entradas e das saidas tem que ser ==1
 bezinho = np.zeros(n,int)
-bezinho[ini-1]= peso
+bezinho[ini-1]= -1*peso
 bezinho[fim-1]= peso
 
 azinho = []
@@ -41,15 +46,9 @@ for i in range (0,len(lista),3):
     j=aux[0]+aux[1]
     for no in range(1,n+1):
         if int(aux[0])==no:
-            if ini<fim:
                 azinho[no-1][j]= -1 # os if definem a orientação do grafo baseado do pontp de saida e entreada
-            else:
-                azinho[no-1][j]= 1
         if int(aux[1])==no:
-            if ini<fim:
                 azinho[no-1][j]= 1
-            else:
-                azinho[no-1][j]= -1
 
 #transformando o dicionario em um array
 nokeys= list()
@@ -63,14 +62,8 @@ nokeys2.append(cezinho.values())
 #as entradas e as saidas são uma soma, arruma com o -1
 azinho = np.copy(nokeys)
 #azinho[ini-1] = -1 * azinho[ini-1]
-for i in range(0,e):
-    if int(azinho[ini-1][i]) == -1:
-        azinho[ini-1][i] = 1
-for i in range(0,e):
-    if int(azinho[fim-1][i]) == -1:
-        azinho[fim-1][i] = 1
 
-cezinho = np.copy(nokeys2)
+cezinho = np.copy(nokeys2[0])
 #restrições
 a = azinho
 #respostas das equações das restrições
@@ -82,9 +75,16 @@ c = cezinho
 #print(nos.keys())
 #print(azinho)
 #print(bezinho)
-print(cezinho)
+#print(cezinho)
 
 
 from scipy.optimize import linprog
-res = linprog(c, A_eq=a, b_eq=b, method='simplex')
-print(res.fun)
+res = linprog(c, A_eq=a, b_eq=b, method='simplex',bounds=(None,None))
+listax = res.x
+resposta = 0
+
+# aqui eu calculo com os pesos e os X absolutos
+for i in range(0,e):
+    resposta = resposta + abs(listax[i]) * int(c[i])
+
+print(resposta)
