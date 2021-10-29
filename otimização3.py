4da7bf912cde41e5e04dd3307ab3e37b0917231e
ojsj18 <ojsj18@gmail.com>
2021-10-29 20:38:00 -0300

    terminando

--- otimização3.py
+++ otimização3.py
@@ -25,51 +25,43 @@ bezinho = np.zeros(n,int)
 bezinho[ini-1]= -1*peso
 bezinho[fim-1]= peso
 
-azinho = []
-cezinho = []
-
+azinho = np.zeros((n,e),int)
+cezinho = dict()  
 # se estiver saindo do nó == -1 se estiver chegando no nó == 1
 for i in range (0,len(lista),3):
     aux=lista[i:i+3]
-    j=aux[0]+aux[1]
-    k=aux[1]+aux[0]
+    j=int(aux[0])
+    k=int(aux[1])
+    x = str(aux[0]+aux[1])
+    z = str(aux[1]+aux[0])
     for no in range(1,n+1):
         if int(aux[0])==no:
-            azinho[no-1][j]= "1" # os if definem a orientação do grafo baseado do ponto de saida e entrada
-            azinho[no-1][k]= -1
+            azinho[no-1][k-1]= 1 # os if definem a orientação do grafo baseado do ponto de saida e entrada
         if int(aux[1])==no:
-            azinho[no-1][j]= -1
-            azinho[no-1][k]= "1"
+            azinho[k-1][j-1]= 1
+    cezinho[x]=aux[2]
+    cezinho[z]=aux[2]
 
-#transformando o dicionario em um array
-nokeys= list()
-nokeys2= list()
-
-for i in range(0,len(azinho)):
-    nokeys.append(azinho[i].values())
-
-nokeys2.append(cezinho.values())
-
-#as entradas e as saidas são uma soma, arruma com o -1
-azinho = np.copy(nokeys)
-cezinho = np.copy(nokeys2[0])
 #restrições
 a = azinho
 #respostas das equações das restrições
 b = bezinho
 #função objetivo
 c = cezinho
-
-indices = nos.keys()
 linha = str()
 objetiva = "min:"
-for j in range(0,e*2):
-    objetiva = objetiva+" "+cezinho[j]+"x"+indices[j]+" "
+lista = cezinho.values()
+lista2 = cezinho.keys()
+
+for i in range(0,e*2):
+    objetiva = objetiva+str(lista[i])+"x"+str(lista2[i])+" "
 objetiva = objetiva+";"
 print(objetiva)
-for j in range(0,n):
-    for i in range(0,e*2):
-        if int(azinho[j][i]) != 0:
-            linha = linha+str(azinho[j][i])+"x"+indices[i]+" "
-    linha= linha+"= "+str(bezinho[j])+";"+"\n"
+
+for i in range(0,n):
+    for j in range(0,e):
+        if int(azinho[i][j]) != 0:
+            linha = linha+" x"+str(i+1)+str(j+1)+" "
+            linha = linha+"- x"+str(j+1)+str(i+1)+" "
+    linha= linha+"= "+str(bezinho[i])+";"+"\n"
 print(linha)
